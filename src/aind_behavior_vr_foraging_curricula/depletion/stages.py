144d44b0e4d7bcb36e9fe2fc0463ea93d0adb534
tiffany.ona <tiffany.ona@gmail.com>
2025-10-15 17:42:56 -0700

    add the right stages

--- src/aind_behavior_vr_foraging_curricula/depletion/stages.py
+++ src/aind_behavior_vr_foraging_curricula/depletion/stages.py
@@ -9,46 +9,47 @@ from .policies import p_learn_to_run, p_learn_to_stop, p_stochastic_reward
 # ============================================================
 # Stage definition
 # ============================================================
-
-s_stage_one_odor_no_depletion = Stage(
-    name="one_odor_no_depletion",
-    task=AindVrForagingTaskLogic(
-        stage_name="one_odor_no_depletion",
-        task_parameters=AindVrForagingTaskParameters(
-            updaters={
+updaters = {
                 task_logic.UpdaterTarget.STOP_DURATION_OFFSET: task_logic.NumericalUpdater(
                     operation=task_logic.NumericalUpdaterOperation.OFFSET,
                     parameters=task_logic.NumericalUpdaterParameters(
-                        initial_value=0, on_success=0.003, minimum=0, maximum=0.6
+                        initial_value=0, on_success=0.0005, minimum=0, maximum=0.5
                     ),
                 ),
                 task_logic.UpdaterTarget.REWARD_DELAY_OFFSET: task_logic.NumericalUpdater(
                     operation=task_logic.NumericalUpdaterOperation.OFFSET,
                     parameters=task_logic.NumericalUpdaterParameters(
                         initial_value=0,
-                        on_success=-0.002,
+                        on_success=-0.003,
                         minimum=0,
-                        maximum=0.4,
+                        maximum=0.5,
                     ),
                 ),
                 task_logic.UpdaterTarget.STOP_VELOCITY_THRESHOLD: task_logic.NumericalUpdater(
                     operation=task_logic.NumericalUpdaterOperation.GAIN,
                     parameters=task_logic.NumericalUpdaterParameters(
                         initial_value=60,
-                        on_success=0.99,
+                        on_success=0.96,
                         minimum=10,
                         maximum=60,
                     ),
                 ),
             },
-            operation_control=helpers.make_default_operation_control(time_to_collect=99999, velocity_threshold=0),
+
+s_stage_one_odor_no_depletion = Stage(
+    name="one_odor_no_depletion",
+    task=AindVrForagingTaskLogic(
+        stage_name="one_odor_no_depletion",
+        task_parameters=AindVrForagingTaskParameters(
+            updaters=updaters,
+            operation_control=helpers.make_default_operation_control(time_to_collect=99999, velocity_threshold=60),
             environment=task_logic.BlockStructure(
                 blocks=[
                     task_logic.Block(
                         environment_statistics=task_logic.EnvironmentStatistics(
                             patches=[
                                 task_logic.Patch(
-                                    label="odor_0",
+                                    label="PatchZA",
                                     state_index=0,
                                     odor_specification=task_logic.OdorSpecification(index=0, concentration=1),
                                     reward_specification=task_logic.RewardSpecification(
@@ -57,17 +58,11 @@ s_stage_one_odor_no_depletion = Stage(
                                         available=task_logic.scalar_value(9999),
                                         reward_function=[],
                                     ),
-                                    patch_virtual_sites_generator=task_logic.PatchVirtualSitesGenerator(
-                                        inter_patch=helpers.make_interpatch(
-                                            length_distribution=helpers.exponential_distribution(100, 150)  # TODO
-                                        ),
-                                        inter_site=helpers.make_intersite(
-                                            length_distribution=helpers.exponential_distribution(15, 25)  # TODO
-                                        ),
-                                        reward_site=helpers.make_reward_site(
-                                            length_distribution=task_logic.scalar_value(20)
-                                        ),
-                                    ),
+                                    patch_virtual_sites_generator=helpers.make_virtualsites (rewardsite = 20, 
+                                                                                             interpatch_min = 50, 
+                                                                                             interpatch_max = 100,
+                                                                                             intersite_min = 20,
+                                                                                             intersite_max = 40)
                                 )
                             ]
                         ),
@@ -81,26 +76,112 @@ s_stage_one_odor_no_depletion = Stage(
     metrics_provider=MetricsProvider(metrics_from_dataset),
 )
 
+s_stage_one_odor_w_depletion_parameters = AindVrForagingTaskParameters(
+            updaters= updaters,
+            operation_control=helpers.make_default_operation_control(time_to_collect=99999, velocity_threshold=8),
+            environment=task_logic.BlockStructure(
+                blocks=[
+                    task_logic.Block(
+                        environment_statistics=task_logic.EnvironmentStatistics(
+                            patches=[
+                                task_logic.Patch(
+                                    label="PatchZB",
+                                    state_index=0,
+                                    odor_specification=task_logic.OdorSpecification(index=0, concentration=1),
+                                    reward_specification=helpers.CountUntilDepleted(available_water= 9999, 
+                                                        amount_drop=5, 
+                                                        max_p=0.9,
+                                                        stop_duration=0.0),
+                                    patch_virtual_sites_generator=helpers.make_virtualsites()
+                                )
+                            ]
+                        ),
+                        end_conditions=[],
+                    )
+                ],
+            ),
+        )
+    
 s_stage_one_odor_w_depletion_day_0 = Stage(
     name="one_odor_w_depletion_day_0",
-    task=AindVrForagingTaskLogic(),
+    task=AindVrForagingTaskLogic(        
+        stage_name="one_odor_no_depletion_day_0",
+        task_parameters=s_stage_one_odor_w_depletion_parameters,
+    ),
     metrics_provider=MetricsProvider(metrics_from_dataset),
 )
 
 s_stage_one_odor_w_depletion_day_1 = Stage(
     name="one_odor_w_depletion_day_1",
-    task=AindVrForagingTaskLogic(),
+    task=AindVrForagingTaskLogic( stage_name="one_odor_no_depletion_day_1",
+        task_parameters=s_stage_one_odor_w_depletion_parameters,
+    ),
     metrics_provider=MetricsProvider(metrics_from_dataset),
 )
 
 s_stage_all_odors_rewarded = Stage(
     name="all_odors_rewarded",
-    task=AindVrForagingTaskLogic(),
+    task=AindVrForagingTaskLogic(AindVrForagingTaskParameters(
+            updaters= updaters,
+            operation_control=helpers.make_default_operation_control(time_to_collect=99999, velocity_threshold=8),
+            environment=task_logic.BlockStructure(
+                blocks=[
+                    task_logic.Block(
+                        environment_statistics=task_logic.EnvironmentStatistics(
+                            first_state_occupancy = [0.5,0.5], 
+                            transition_matrix=[[0.5,0.5], [0.5,0.5]],
+                            patches=[
+                                helpers.make_graduated_patch(label = 'PatchB',
+                                                             state_index=0,
+                                                             odor_index=1, 
+                                                             max_reward_probability=0.9),
+                                helpers.make_graduated_patch(label = 'PatchC',
+                                                             state_index=1,
+                                                             odor_index=2, 
+                                                             max_reward_probability=0.6)
+                            ]
+                        ),
+                        end_conditions=[],
+                    )
+                ],
+            ),
+        ),
+    ),
     metrics_provider=MetricsProvider(metrics_from_dataset),
 )
 
 s_stage_graduation = Stage(
     name="graduation",
-    task=AindVrForagingTaskLogic(),
+    task=AindVrForagingTaskLogic(AindVrForagingTaskParameters(
+            updaters= updaters,
+            operation_control=helpers.make_default_operation_control(time_to_collect=99999, velocity_threshold=8),
+            environment=task_logic.BlockStructure(
+                blocks=[
+                    task_logic.Block(
+                        environment_statistics=task_logic.EnvironmentStatistics(
+                            first_state_occupancy=[0.1,0.45,0.45], 
+                            transition_matrix=[[0.1, 0.45, 0.45], [0.1, 0.45, 0.45],[0.1, 0.45, 0.45]],
+                            patches=[
+                                helpers.make_graduated_patch(label = 'odor_0',
+                                                             state_index=0,
+                                                             odor_index=0, 
+                                                             max_reward_probability=0.0),
+                                helpers.make_graduated_patch(label = 'odor_90',
+                                                             state_index=1,
+                                                             odor_index=1, 
+                                                             max_reward_probability=0.9),
+                                helpers.make_graduated_patch(label = 'odor_60',
+                                                             state_index=2,
+                                                             odor_index=2, 
+                                                             max_reward_probability=0.6),
+
+                            ]
+                        ),
+                        end_conditions=[],
+                    )
+                ],
+            ),
+        ),
+    ),
     metrics_provider=MetricsProvider(metrics_from_dataset),
 )
