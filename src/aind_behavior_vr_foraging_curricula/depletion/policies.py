3f7a8409598281b243bff41a482320a5b039dc55
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-11-22 13:17:57 -0800

    Use mutable references and clean-up code

--- src/aind_behavior_vr_foraging_curricula/depletion/policies.py
+++ src/aind_behavior_vr_foraging_curricula/depletion/policies.py
@@ -18,96 +18,80 @@ PolicyType = Callable[
 
 
 def p_stochastic_reward(metrics: DepletionCurriculumMetrics, task: AindVrForagingTaskLogic) -> AindVrForagingTaskLogic:
-    if metrics.total_water_consumed > 750:
-        task.task_parameters.environment.blocks[0].environment_statistics.patches[
-            0
-        ].reward_specification.probability = task_logic.scalar_value(0.9)
+    if metrics.total_water_consumed > 0.750:
+        assert len(task.task_parameters.environment.blocks) > 0, "Expected at least one block in the task parameters"
+        default_block = task.task_parameters.environment.blocks[0]
+        assert len(default_block.environment_statistics.patches) > 0, "Expected at least one patch in the default block"
 
-        task.task_parameters.environment.blocks[0].environment_statistics.patches[
-            0
-        ].reward_specification.reward_function[0].probability = task_logic.scalar_value(0.9)
+        default_block.environment_statistics.patches[0].reward_specification.probability = task_logic.scalar_value(0.9)
 
-        rf = (
-            task.task_parameters.environment.blocks[0]
-            .environment_statistics.patches[0]
-            .reward_specification.reward_function
-        )
+        reward_functions_defined = default_block.environment_statistics.patches[0].reward_specification.reward_function
 
-        for i, f in enumerate(rf):
-            if f.function_type == "PatchRewardFunction":
-                rf[i].probability = task_logic.SetValueFunction(value=task_logic.scalar_value(0.9))
+        for reward_function in reward_functions_defined:
+            if isinstance(reward_function, task_logic.PatchRewardFunction):
+                reward_function.probability = task_logic.SetValueFunction(value=task_logic.scalar_value(0.9))
 
     return task
 
 
 def p_learn_to_run(metrics: DepletionCurriculumMetrics, task: AindVrForagingTaskLogic) -> AindVrForagingTaskLogic:
-    if metrics.n_reward_sites_travelled > 200:
-        patch_gen = (
-            task.task_parameters.environment.blocks[0].environment_statistics.patches[0].patch_virtual_sites_generator
+    if metrics.n_reward_sites_traveled > 200:
+        assert len(task.task_parameters.environment.blocks) > 0, "Expected at least one block in the task parameters"
+        default_block = task.task_parameters.environment.blocks[0]
+        assert len(default_block.environment_statistics.patches) > 0, "Expected at least one patch in the default block"
+        patch_gen = default_block.environment_statistics.patches[0].patch_virtual_sites_generator
+
+        gain = min(metrics.n_reward_sites_traveled / 200.0, 3.0)  # 1.0 for 200, 2.0 for 400, 3.0 for 600+
+
+        assert isinstance(patch_gen.inter_site.length_distribution, distributions.ExponentialDistribution)
+        assert patch_gen.inter_site.length_distribution.truncation_parameters is not None
+        patch_gen.inter_site.length_distribution.truncation_parameters.min = helpers.clamp(
+            patch_gen.inter_site.length_distribution.truncation_parameters.min * (1.5**gain),
+            minimum=10,
+            maximum=20,
         )
-        n_times = metrics.n_reward_sites_travelled // 200  # 1 for 200–399, 2 for 400–599, 3 for 600+
-        n_times = min(n_times, 3)
-        for _ in range(n_times):
-            assert isinstance(patch_gen.inter_site.length_distribution, distributions.ExponentialDistribution)
-            assert patch_gen.inter_site.length_distribution.truncation_parameters is not None
-            patch_gen.inter_site.length_distribution.truncation_parameters.min = helpers.clamp(
-                patch_gen.inter_site.length_distribution.truncation_parameters.min * 1.5,
-                minimum=10,
-                maximum=20,
-            )
-            patch_gen.inter_site.length_distribution.truncation_parameters.max = helpers.clamp(
-                patch_gen.inter_site.length_distribution.truncation_parameters.max * 1.5,
-                minimum=30,
-                maximum=100,
-            )
+        patch_gen.inter_site.length_distribution.truncation_parameters.max = helpers.clamp(
+            patch_gen.inter_site.length_distribution.truncation_parameters.max * (1.5**gain),
+            minimum=30,
+            maximum=100,
+        )
 
-            assert isinstance(patch_gen.inter_patch.length_distribution, distributions.ExponentialDistribution)
-            assert patch_gen.inter_patch.length_distribution.truncation_parameters is not None
-            patch_gen.inter_patch.length_distribution.truncation_parameters.min = helpers.clamp(
-                patch_gen.inter_patch.length_distribution.truncation_parameters.min * 2,
-                minimum=25,
-                maximum=200,
-            )
-            patch_gen.inter_patch.length_distribution.truncation_parameters.max = helpers.clamp(
-                patch_gen.inter_patch.length_distribution.truncation_parameters.max * 2,
-                minimum=75,
-                maximum=600,
-            )
+        assert isinstance(patch_gen.inter_patch.length_distribution, distributions.ExponentialDistribution)
+        assert patch_gen.inter_patch.length_distribution.truncation_parameters is not None
+        patch_gen.inter_patch.length_distribution.truncation_parameters.min = helpers.clamp(
+            patch_gen.inter_patch.length_distribution.truncation_parameters.min * (2**gain),
+            minimum=25,
+            maximum=200,
+        )
+        patch_gen.inter_patch.length_distribution.truncation_parameters.max = helpers.clamp(
+            patch_gen.inter_patch.length_distribution.truncation_parameters.max * (2**gain),
+            minimum=75,
+            maximum=600,
+        )
 
-            assert isinstance(patch_gen.reward_site.length_distribution, distributions.Scalar)
-            patch_gen.reward_site.length_distribution.distribution_parameters.value = helpers.clamp(
-                patch_gen.reward_site.length_distribution.distribution_parameters.value + 10,
-                minimum=20,
-                maximum=50,
-            )
-            # This should not be needed, but just in case...
-            task.task_parameters.environment.blocks[0].environment_statistics.patches[
-                0
-            ].patch_virtual_sites_generator = patch_gen
+        assert isinstance(patch_gen.reward_site.length_distribution, distributions.Scalar)
+        patch_gen.reward_site.length_distribution.distribution_parameters.value = helpers.clamp(
+            patch_gen.reward_site.length_distribution.distribution_parameters.value + (10 * gain),
+            minimum=20,
+            maximum=50,
+        )
 
     return task
 
 
 def p_learn_to_stop(metrics: DepletionCurriculumMetrics, task: AindVrForagingTaskLogic) -> AindVrForagingTaskLogic:
     if metrics.n_choices > 100:
-        task.task_parameters.updaters[task_logic.UpdaterTarget.STOP_VELOCITY_THRESHOLD].parameters.initial_value = (
-            task.task_parameters.updaters[task_logic.UpdaterTarget.STOP_VELOCITY_THRESHOLD].parameters.initial_value
-            - 16.6
+        updaters = task.task_parameters.updaters
+        updaters[task_logic.UpdaterTarget.STOP_VELOCITY_THRESHOLD].parameters.initial_value = (
+            updaters[task_logic.UpdaterTarget.STOP_VELOCITY_THRESHOLD].parameters.initial_value - 16.6
         )
-        if (
-            task.task_parameters.updaters[task_logic.UpdaterTarget.STOP_VELOCITY_THRESHOLD].parameters.initial_value
-            < 15.2
-        ):
-            task.task_parameters.updaters[
-                task_logic.UpdaterTarget.STOP_VELOCITY_THRESHOLD
-            ].parameters.initial_value = 15
+        if updaters[task_logic.UpdaterTarget.STOP_VELOCITY_THRESHOLD].parameters.initial_value < 15.2:
+            updaters[task_logic.UpdaterTarget.STOP_VELOCITY_THRESHOLD].parameters.initial_value = 15
 
-        task.task_parameters.updaters[task_logic.UpdaterTarget.STOP_DURATION_OFFSET].parameters.initial_value = (
-            task.task_parameters.updaters[task_logic.UpdaterTarget.STOP_DURATION_OFFSET].parameters.initial_value + 0.1
+        updaters[task_logic.UpdaterTarget.STOP_DURATION_OFFSET].parameters.initial_value = (
+            updaters[task_logic.UpdaterTarget.STOP_DURATION_OFFSET].parameters.initial_value + 0.1
         )
 
-        task.task_parameters.updaters[
-            task_logic.UpdaterTarget.REWARD_DELAY_OFFSET
-        ].parameters.initial_value = metrics.last_delay_duration
+        updaters[task_logic.UpdaterTarget.REWARD_DELAY_OFFSET].parameters.initial_value = metrics.last_delay_duration
 
     return task
