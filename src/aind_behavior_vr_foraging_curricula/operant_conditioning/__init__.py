2a9222fa0e524142a4d624bd7ce38592f454e286
Bruno Cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2026-03-03 11:32:28 -0800

    Revert "Add curriculum for operant conditioning experiments"

--- src/aind_behavior_vr_foraging_curricula/operant_conditioning/__init__.py
+++
@@ -1,9 +0,0 @@
-from .curriculum import CURRICULUM, CURRICULUM_NAME, PKG_LOCATION, TRAINER, run_curriculum
-
-__all__ = [
-    "CURRICULUM_NAME",
-    "CURRICULUM",
-    "TRAINER",
-    "run_curriculum",
-    "PKG_LOCATION",
-]
